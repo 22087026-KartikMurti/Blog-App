9a4c0a0c9d8de4b723fd3f24a2268aa76c6888de
Kartik Murti <mkartikkmurti@gmail.com>
2025-06-09 13:51:33 +1000

    Tests added

---
+++ tests/playwright/tests/auth.setup.ts
@@ -0,0 +1,26 @@
+import { test as setup } from "@playwright/test";
+import fs from "fs";
+
+////////////////////////////////////////////////////////
+// Authentication for Assignment 3
+// Uncomment once you start working on the assignment 3
+////////////////////////////////////////////////////////
+
+setup(
+  "authenticate assignment 3",
+  { tag: "@a3" },
+  async ({ playwright }) => {
+    const authFile = ".auth/user.json";
+
+    const apiContext = await playwright.request.newContext();
+
+    await apiContext.post("/api/auth", {
+      data: JSON.stringify({ password: "123" }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    await apiContext.storageState({ path: authFile });
+  },
+);
