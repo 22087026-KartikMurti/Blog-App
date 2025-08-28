7e9beaa2cf22c06c328dbd2ef4c132853a21aa17
Kartik Murti <mkartikkmurti@gmail.com>
2025-08-28 15:32:28 +1000

    feat: add API endpoints for liking and unliking posts with validation

---
+++ apps/admin/src/app/api/likes/[postId]/route.ts
@@ -0,0 +1,203 @@
+import { NextResponse, NextRequest } from "next/server";
+import { client } from "@repo/db/client";
+import { headers } from "next/headers";
+
+export async function GET(
+    request: NextRequest,
+    context: { params: Promise<{ postId: string }> }
+): Promise<NextResponse> {
+    try {
+      const pId = await context.params;
+      const headersList = await headers();
+      const ip = headersList.get('x-forwarded-for') || 
+              request.headers.get('x-real-ip') ||
+              '127.0.0.1';
+
+      if (!ip) {
+        return NextResponse.json({ error: "IP address not found" }, { status: 400 });
+      }
+
+      //Check if the request is coming from the intended origin
+      const origin = headersList.get('origin');
+      const host = headersList.get('host');
+      const isValidOrigin = !origin || origin === `https://${host}`;
+      if(!isValidOrigin) {
+        return NextResponse.json({ error: "Invalid request origin" }, { status: 403 });
+      }
+
+      const postId = parseInt(pId.postId);
+      
+      if (isNaN(postId)) {
+        return NextResponse.json({ error: "Invalid post ID" }, { status: 400 });
+      }
+      
+      // Check if this IP has liked this post
+      const existingLike = await client.db.like.findFirst({
+        where: {
+            postId,
+            userIP: ip.toString(),
+        },
+      });
+
+      return NextResponse.json({ 
+          isLiked: !!existingLike
+      });
+    } catch (error) {
+      console.error("Error fetching liked or not: ", error);
+      return NextResponse.json(
+          { error: "Failed to fetch liked or not" },
+          { status: 500 }
+      );
+    }
+  }
+export async function POST(
+    request: NextRequest,
+    context: { params: Promise<{ postId: string}> }
+  ): Promise<NextResponse> {
+    const pId = await context.params;
+    const headersList = await headers();
+    const ip = headersList.get('x-forwarded-for') ||
+            request.headers.get('x-real-ip') ||
+            '127.0.0.1';
+
+    if (!ip) {
+        return NextResponse.json({ error: "IP address not found" }, { status: 400 });
+    }
+    
+    try {
+        const postId = parseInt(pId.postId);
+        if (isNaN(postId)) {
+          return NextResponse.json({ error: "Invalid post ID" }, { status: 400 });
+        }
+        
+        //Check if the request is coming from the intended origin
+        const origin = headersList.get('origin');
+        const host = headersList.get('host');
+        const isValidOrigin = !origin || origin === `http://${host}`;
+        if(!isValidOrigin) {
+          return NextResponse.json({ error: "Invalid request origin" }, { status: 403 });
+        }
+                
+        // Check if this IP has liked this post
+        const existingLike = await client.db.like.findFirst({
+          where: {
+              postId,
+              userIP: ip.toString(),
+          },
+        });
+    
+        if(existingLike) {
+          return NextResponse.json({
+              error: "Already liked",
+              status: 409,
+          });
+        }
+        try {
+          await client.db.like.create({
+              data: {
+                  postId,
+                  userIP: ip.toString(),
+              },
+          });
+        } catch (error) {
+          console.error("Error creating like relation in db: ", error);
+          return NextResponse.json(
+              { error: "Failed to create like relation in db" },
+              { status: 500 }
+          );
+        }
+
+        const likeCount = await client.db.like.count({
+          where: {
+            postId,
+          },
+        });
+        return NextResponse.json({ likes: likeCount }, { status: 200 });
+
+    } catch (error) {
+      console.error("Error liking post: ", error);
+      return NextResponse.json(
+        { error: "Failed to like post" },
+        { status: 500 }
+      );
+    }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  context: { params: Promise<{ postId: string }> }
+): Promise<NextResponse> {
+  try {
+    const pId = await context.params;
+    // Get client IP address for tracking likes
+    const headersList = await headers();
+    const ip = headersList.get('x-forwarded-for') || 
+            request.headers.get('x-real-ip') || 
+            '127.0.0.1';
+
+    if (!ip) {
+      return NextResponse.json({ error: "IP address not found" }, { status: 400 });
+    }
+
+    //Check if the request is coming from the intended origin
+    const origin = headersList.get('origin');
+    const host = headersList.get('host');
+    const isValidOrigin = !origin || origin === `http://${host}`;
+    if(!isValidOrigin) {
+      return NextResponse.json({ error: "Invalid request origin" }, { status: 403 });
+    }
+
+    const postId = parseInt(pId.postId);
+    
+    if (isNaN(postId)) {
+      return NextResponse.json({ error: "Invalid post ID" }, { status: 400 });
+    }
+    
+    // Check if this IP has liked this post
+    const existingLike = await client.db.like.findFirst({
+      where: {
+        postId,
+        userIP: ip.toString(),
+      },
+    });
+
+    if(!existingLike) {
+      return NextResponse.json({
+        error: "Not liked yet",
+        status: 409,
+      });
+    }
+
+    try {
+        await client.db.like.delete({
+          where: {
+            postId_userIP: {
+              postId: existingLike.postId,
+              userIP: ip.toString(),
+            }
+          }
+        });
+    } catch (error) {
+      console.error("Error deleting like relation in db: ", error);
+      return NextResponse.json(
+        { error: "Failed to delete like relation in db" },
+        { status: 500 }
+      );
+    }
+
+    const likeCount = await client.db.like.count({
+      where: {
+        postId,
+      },
+    });
+
+    return NextResponse.json({ likes: likeCount }, { status: 200 });
+
+  } catch (error) {
+    console.error("Error unliking post: ", error);
+    return NextResponse.json(
+      { error: "Failed to unliking post" },
+      { status: 500 }
+    );
+  }
+}
